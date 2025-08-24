2a436d538881963674d948f5073c85fd986d4bf7
oleina <oleina@google.com>
2025-08-24 12:07:22 -0700

    finish day 1

--- src/Day01.hs
+++ src/Day01.hs
@@ -22,9 +22,17 @@ part2 input =
      in
         sum $ map ((\a -> getFreq a * a) . fst) input
 
+readInt :: String -> Int
+readInt = read
+
 day01 :: IO ()
 day01 = do
   inputLines <- lines <$> (getDataFileName "day01-input.txt" >>= readFile)
-  putStrLn "This is what I read from input:"
-  putStrLn $ unlines inputLines
-  putStrLn "TODO: implement Day 01"
+  let tuplify wds = (readInt $ head wds, readInt $ last wds)
+  let asTuples = map (tuplify . words) inputLines
+
+  putStrLn "Part 1:"
+  print $ part1 asTuples
+
+  putStrLn "Part 2:"
+  print $ part2 asTuples
